a317cbff3b922d8fc28051706c964e4836f1952f
jskinner <jonathan@jskinner.info>
2026-01-21 12:33:05 -0700

    implemented json parsing

--- internal/json/json.go
+++ internal/json/json.go
@@ -1,10 +1,20 @@
 package json
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 )
 
-func Parse(input io.Reader) (interface{}, error) {
-	return nil, fmt.Errorf("not implemented")
+func Parse(input io.Reader) (any, error) {
+	b, err := io.ReadAll(input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read input data: %w", err)
+	}
+	data := make(map[string]any)
+	err = json.Unmarshal(b, &data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal input data: %w", err)
+	}
+	return data, nil
 }
